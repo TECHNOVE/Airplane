71d07999159ed4e63334f924b4c5aad4d8ecbcef
Paul Sauve <paul@technove.co>
2021-04-23 11:59:19 -0500

    Update gradle configuration

--- subprojects/server.gradle.kts
+++ subprojects/server.gradle.kts
@@ -1,10 +1,5 @@
-import xyz.jpenilla.toothpick.loadDependencies
-import xyz.jpenilla.toothpick.loadRepositories
-
 repositories {
-    loadRepositories(project)
 }
 
 dependencies {
-    loadDependencies(project)
 }
