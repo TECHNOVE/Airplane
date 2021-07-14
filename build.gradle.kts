d212c718b4aae6130d16b63b8ce1153a4929b4e1
Paul <paul@technove.co>
2021-07-14 09:38:41 -0500

    Revert paperweight version once again

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     id("com.github.johnrengelman.shadow") version "7.0.0" apply false
-    id("io.papermc.paperweight.patcher") version "1.1.9-SNAPSHOT"
+    id("io.papermc.paperweight.patcher") version "1.1.6"
 }
 
 repositories {
